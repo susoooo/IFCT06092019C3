e5afb74585cb75e2675243be3f2c7322453c1d2a
Tere <teresa@hotmail.com>
2020-02-07 12:15:03 +0100

    7/02

---
+++ Tere/hola.c
@@ -0,0 +1,20 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/kern_levels.h>
+
+
+static int hola_init(void){
+	printk(KERN_EMERG "hola mundo kernel!!!\n");
+return(0);
+}
+
+static void hola_exit(void){
+printk(KERN_ALERT "Boas noites kernel!!!\n");
+
+}
+
+module_init (hola_init);
+module_exit (hola_exit);
+
+MODULE_LICENSE("Dual BSD/GPL");
