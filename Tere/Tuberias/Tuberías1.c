bdf18188dad99da9126e5c79e3b54c4fba5c8cc4
Tere <teresa@hotmail.com>
2020-02-12 09:42:56 +0100

    12 de febrero

--- Tere/Tuberias/Tuberías1.c
+++
@@ -1,4 +0,0 @@
-/* 1.Escribir un programa que cree un proceso padre y un proceso hijo.
-Entre ellos se establecerá una tubería,
-de tal forma que uno de ellos pedirá datos por pantalla al usuario,
-se los enviará al otro proceso, y este los imprimirá por pantalla. */
