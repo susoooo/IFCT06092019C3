ab0040bafb12e11b97a389461b64dde00ec12455
jesusp <jpenacastedo@gmail.com>
2020-02-20 13:49:08 +0100

    jueves20

--- jesusp/calcuclien.c
+++ jesusp/calcuclien.c
@@ -15,10 +15,10 @@ char hostname[64];
 int s, n, len;
 int num1;
 int num2;
-int opcion;
+int *resul;
+int *opcion;
 struct sockaddr_in name;
 struct hostent *hp;
-
 
 gethostname(hostname, sizeof(hostname));
 hp = gethostbyname(hostname);
@@ -29,34 +29,36 @@ memcpy(&name.sin_addr, hp->h_addr_list[0], hp->h_length);
 len = sizeof(struct sockaddr_in);
 
 printf("Dame un 0 o 1");
-scanf("%d",&opcion);
-sendto(s, &opcion, n, 0, (struct sockaddr*) &name, len);
-while ( (n = read(0, buf, sizeof(buf))) > 0)
+scanf("%d",opcion);
+sendto(s, &*opcion, n, 0, (struct sockaddr*) &name, len);
+while ( (n = read(0, buf, 1024)) > 0)
     {
-    switch(opcion)
+    switch(*opcion)
         {
             case 0:
-            printf("Hago suma");
-            printf("Primer sumando");
-            scanf("%",&num1);
-            printf("Segundo sumando");
-            scanf("%",&num2);
-            sendto(s, &num1, &num2, 0, (struct sockaddr*) &name, len);
-            recvfrom(s,buf, sizeof(buf),0, (struct sockaddr*) &name, &len);
-            n = recv(s, num1, num2, sizeof(opcion), 0);
+            printf("Hago suma\n");
+            printf("Primer sumando: \n");
+            scanf("%d",&num1);
+            sendto(s, &num1, sizeof(num1), 0, (struct sockaddr*) &name, len);
             sleep(1);
-            write(1, num1, num2, n);
+
+            printf("Segundo sumando");
+            scanf("%d",&num2);
+            sendto(s, &num2, sizeof(num2), 0, (struct sockaddr*) &name, len);
+            recvfrom(s,resul, sizeof(resul),0, (struct sockaddr*) &name, &len);
+            write(1, resul,sizeof(resul));
+            sleep(1);
             break;
 
             case 1:
-            printf("Hago factorial");
-            printf("Dame un numero");
+            printf("Hago factorial\n");
+            printf("Dame un numero: ");
             scanf("%d",&num1);
-            sendto(s, &num1, n, 0, (struct sockaddr*) &name, &len);
-            recvfrom(s,buf, sizeof(buf),0, (struct sockaddr*) &name, &len);
-            n = recv(s, num1,  sizeof(opcion), 0);
+            sendto(s, &num1, sizeof(num1), 0, (struct sockaddr*) &name, len);
+            recvfrom(s, resul, sizeof(resul),0, (struct sockaddr*) &name, &len);
+            n = recv(s, resul,  sizeof(resul), 0);
+            write(1, resul, sizeof(resul));
             sleep(1);
-            write(1, num1, n);
             break;
 
             default:
