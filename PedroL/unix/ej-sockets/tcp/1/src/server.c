d4ec9249093890ddb9f2000e5aeee233483c0340
Pedro Luis G <pedroluisgl@live.com>
2020-02-20 13:55:49 +0100

    02-20

--- PedroL/unix/ej-sockets/tcp/1/src/server.c
+++ PedroL/unix/ej-sockets/tcp/1/src/server.c
@@ -13,13 +13,10 @@
 int
 main(void)
 {
-    char buf0[BUFLEN], buf1[BUFLEN];
-    int s, ns, bn, eval, num[2];
+    char bufraw[BUFLEN], bufcln[BUFLEN], bufscr[BUFLEN];
+    int s, ns, br, num[2], eval;
     struct sockaddr_in saddr, nsaddr;
     socklen_t addrlen;
-    pid_t chpid;
-    
-    eval = 0;
 	
     s = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -28,32 +25,53 @@ main(void)
     saddr.sin_addr.s_addr = htonl(INADDR_ANY);
     addrlen = sizeof(struct sockaddr_in);
     
-    bind(s, &saddr, addrlen);
+    bind(s, (const struct sockaddr*)&saddr, addrlen);
     	
-    listen(s, 5);
+    listen(s, 1);
+    ns = accept(s, (struct sockaddr*)&nsaddr, &addrlen);
+    perror("accept");
     		
-    ns = accept(s, &nsaddr, &addrlen);
-    
-    while(bn = recv(ns, buf0, BUFLEN, 0) > 0) {
-	if (buf0 == "0") {
-	    for (int i = 0; i < 2; i++) {
-		memset(buf0, 0, BUFLEN);
-		sprintf(buf0, "num %d:\n", i);
-		bn = strlen(buf0);
-		send(ns, buf0, bn, 0);
+    while (1) {
+	br = recv(ns, bufraw, BUFLEN, 0);
 
-		recv(ns, buf0, BUFLEN, 0);
-		memset(buf1, 0, BUFLEN);
-		strcpy(buf1, buf0);
-		printf("%d\n", buf1);
-		num[i] = atoi(buf1);
+	printf("bufraw: %s\n", bufraw);
+	memset(bufraw, 0, BUFLEN);
+	    perror("recv");
+
+	eval = atoi(bufraw);
+	printf("string to number: %d\n", eval);
+
+	if (eval == 0) {
+		perror("if 0");
+
+	    for (int i = 0; i < 2; i++) {
+		sprintf(bufscr, "num %d: \n", i);
+	        send(ns, bufscr, BUFLEN, 0);
+		memset(bufscr, 0, BUFLEN);
+		    perror("send");
+
+		recv(ns, bufraw, BUFLEN, 0);
+		num[i] = atoi(bufraw);
+		memset(bufraw, 0, BUFLEN);
+		    perror("recv");
+
+		printf("num %d: %d\n", i, num[0]);
 	    }
 
-	    sprintf(buf0, "ok\n");
-	    bn = strlen(buf0);
-	    send(ns, buf0, bn, 0);
+	    sprintf(bufscr, "suma: %d\n", (num[0] + num[1]));
+	    send(ns, bufscr, BUFLEN, 0);
+	    memset(bufscr, 0, BUFLEN);
+		perror("send");
+
+	    break;
+	} else if (eval == 1) {
+	    
+	    /* WIP */
 	}
+
+	usleep(100);
     }
 
+    perror("closing");
     close(s);
 }
