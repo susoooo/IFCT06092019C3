d9ae2e1931c26b7aa3c08c8a91357620fa0681d4
movaiva <ivamova@hotmail.com>
2020-07-28 13:34:59 +0200

    Spring 01

---
+++ movaiva/Java/Spring/SpringWebJson/src/test/java/com/movaiva/springwebJson/SpringwebJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movaiva.springwebJson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
