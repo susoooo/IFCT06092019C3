33cdc153ed504eb8e63747237a463e554ec5463e
Francurso <sdanu@hotmail.com>
2020-07-29 13:49:24 +0200

    salida dia 29

--- Fran/NetBeansProjects/Ejercicios1/src/main/java/ifct0609/ejercicios1/SimpleFrame.java
+++ Fran/NetBeansProjects/Ejercicios1/src/main/java/ifct0609/ejercicios1/SimpleFrame.java
@@ -3,18 +3,22 @@ package ifct0609.ejercicios1;
 
 import javax.swing.JFrame;
 
+
+
 public class SimpleFrame extends JFrame 
 {
     public SimpleFrame() 
     {
-        super("Ventanita 1");
-        setSize(800, 600);
+        super("Alquiler por mes y año");
+        setSize(300, 240);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+        
     }
 
     public static void main(String[] args) 
     {
         SimpleFrame sf = new SimpleFrame();
+        
     }
 }
