c4f46e5bcdea871a6324ce19478ef87df6c1eeae
movaiva <ivamova@hotmail.com>
2020-08-29 11:18:53 +0200

    productManager complete

--- movaiva/Java/Spring/productManager/src/main/java/com/movaiva/productmanager/entity/Product.java
+++ movaiva/Java/Spring/productManager/src/main/java/com/movaiva/productmanager/entity/Product.java
@@ -2,6 +2,7 @@ package com.movaiva.productmanager.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,7 +13,8 @@ import javax.persistence.Table;
 @Table(name="product")
 public class Product implements Serializable {
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
 	private Integer id;
 	private String name;
 	private Double prize;
@@ -28,6 +30,10 @@ public class Product implements Serializable {
 
 	public Integer getId() {
 		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id=id;
 	}
 
 	public String getName() {
