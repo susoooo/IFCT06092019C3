50315608e12c9b06b06a7136c7a5d2ac098cad5c
mi usuario <nose@midireccion.com>
2020-09-07 13:34:25 +0200

    deploy

---
+++ santiago/Proyecto/Deploy/migraciones/2020_09_07_074215_create_empresa_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmpresaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresa', function (Blueprint $table) {
+            $table->id();            
+			$table->string('nombre_de_empresa');
+			$table->string('direccion');
+			$table->string('lugar');
+			$table->string('provincia');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+			$table->integer('numeroTelefono');
+			$table->integer('segundoNTeléf');          
+			$table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empresa');
+    }
+}
