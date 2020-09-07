617c57db3f24a6e446d5785de4ca26d328939648
Francurso <sdanu@hotmail.com>
2020-09-07 11:02:48 +0200

    commit 07/09

---
+++ Fran/Proyecto Tron/Tron 0.1.1/Assets/MenuPrincipal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuPrincipal : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    public void Salir()
+    {
+        Application.Quit();
+    }
+
+
+}
