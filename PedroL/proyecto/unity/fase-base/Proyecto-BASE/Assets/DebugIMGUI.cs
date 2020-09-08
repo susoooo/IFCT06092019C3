16cc78a4ada718cdd77c025040ce48543f4b80a1
Pedro Luis Gonzalez <pedroluisgl@live.com>
2020-09-08 13:47:34 +0200

    08/09: 	Added Key object with Key tag 	Renamed scripts/classes 		"Player" to "PlayerMovement" 	Added script 		"PlayerKey" 	Fixed jump 		Only triggers on button down 		Only triggers when touching ground 		Increased jump force to compensate 	Using MainCamera instead of Player´s camera component 	Added collision event handler in PlayerKey 		Deactivates KeyObject and increments key_count 	Added debugging script 		Shows key_count in top left 	Changed camera to third person perspective 	Added win condition 		Satisfied when no keys are left in the world 		Pauses the game and shows a message 	Consolidated PlayerKey.cs into PlayerMovement.cs

---
+++ PedroL/proyecto/unity/fase-base/Proyecto-BASE/Assets/DebugIMGUI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugIMGUI : MonoBehaviour {
+	
+	GameState game_state;
+	// Start is called before the first frame update
+	void Start() {
+		game_state = GameObject.Find("Empty").GetComponent<GameState>();
+		
+	}
+
+	// Update is called once per frame
+	void Update() {
+		
+		
+	}
+	
+	void OnGUI()
+  {
+    GUI.Box(
+			new Rect (0,0,100,50),
+			"keys_taken:\n" + game_state.keys_taken.ToString() 
+		);
+		
+		if (game_state.keys_left == 0) {
+			GUI.Box(
+				new Rect (
+					Screen.width / 2 - 50,
+					Screen.height / 2 - 12,
+					100,
+					24
+				),
+				"ur so good" 
+			);
+		}
+		
+  }
+	
+	void OnDrawGizmos() {
+		Gizmos.DrawLine(Vector3.zero, new Vector3(0.0f, 200.0f, 0.0f));
+		
+	}
+	
+}
---
+++ PedroL/proyecto/unity/fase-base/Proyecto-BASE/Assets/GameState.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour {
+	
+	public int keys_total;
+	public int keys_taken = 0;
+	public int keys_left;
+	
+	// Start is called before the first frame update
+	void Start() {
+		keys_total = GameObject.FindGameObjectsWithTag("Key").Length;
+		keys_left = keys_total;
+		
+		Debug.Log(keys_left);
+		
+	}
+
+    // Update is called once per frame
+	void Update() {
+		
+		
+	}
+	
+	public void update_key_count() {
+		keys_left = GameObject.FindGameObjectsWithTag("Key").Length;
+		
+		keys_taken = keys_total - keys_left;
+		
+		if (keys_left == 0) {
+			Time.timeScale = 0;
+		}
+		
+	}
+	
+}
