efd3a03a250f29f5c70181bbf0a48b16338e1fe9
movaiva <ivamova@hotmail.com>
2020-09-08 13:29:38 +0200

    Proyecto Ivan Moran Version 1.0

--- movaiva/Proyecto/proyecto/src/main/java/com/movaiva/proyecto/repository/OrganizadorRepository.java
+++ movaiva/Proyecto/proyecto/src/main/java/com/movaiva/proyecto/repository/OrganizadorRepository.java
@@ -1,5 +1,8 @@
 package com.movaiva.proyecto.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -9,5 +12,10 @@ public interface OrganizadorRepository extends CrudRepository<Organizador,Intege
 	
 	@Query("select o.id from Organizador o where o.usuario=:usuario and o.email=:email")
 	public Integer findId(String usuario,String email);
+	@Query("select o from Organizador o where o.id=:id")
+	public Optional<Organizador> findById(Integer id);
+	@Modifying
+	@Query("update Organizador o set o.nombre=:nombre, o.apellidos=:apellidos, o.contrasena=:password, o.cuentaBancaria=:cuenta where o.id=:id")
+	public void update(String nombre,String apellidos,String password,String cuenta,Integer id);
 
 }
