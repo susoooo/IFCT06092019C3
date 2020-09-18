d2ac657f5042c3b188deb1c60eee31cc75d6a9d2
unknown <sdanu@hotmail.com>
2020-09-18 22:59:12 +0200

    commit 18/09 casa noche

---
+++ Fran/Proyecto Tron/Tron 0.3/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player1Script : MonoBehaviour
+{
+    public GameObject moto1;
+
+    void Start()
+    {
+        moto1 = Resources.Load("Player1") as GameObject;
+
+        Vector3 randomSpawn = new Vector3(Random.Range(1.0f, 48.0f), 3.5f, Random.Range(1.0f, 48.0f));
+
+        Instantiate(moto1, randomSpawn, transform.rotation);
+    }
+
+    void Update()
+    {
+
+    }
+
+}
