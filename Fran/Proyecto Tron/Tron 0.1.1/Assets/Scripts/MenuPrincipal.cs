0de3d8d9a8b114bf3468c53e7a4461eff31b5f45
Francurso <sdanu@hotmail.com>
2020-09-18 13:18:05 +0200

    commit 18/09

--- Fran/Proyecto Tron/Tron 0.1.1/Assets/Scripts/MenuPrincipal.cs
+++ Fran/Proyecto Tron/Tron 0.1.1/Assets/Scripts/MenuPrincipal.cs
@@ -14,6 +14,7 @@ public class MenuPrincipal : MonoBehaviour
     {
         
     }
+
     public void Salir()
     {
         Application.Quit();
---
+++ Fran/Proyecto Tron/Tron 0.3/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player1Script : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float speed;
+
+    private Vector3 movement;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        movement = new Vector3();
+
+        rb.AddForce(movement * Time.deltaTime);
+    }
+
+}
