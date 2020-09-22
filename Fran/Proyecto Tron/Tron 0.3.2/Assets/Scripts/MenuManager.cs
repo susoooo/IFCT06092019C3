1e2cabe4d377b7b9310e94200a8a651801a49a7a
Francurso <sdanu@hotmail.com>
2020-09-22 13:33:35 +0200

    commit 22/09 salida

---
+++ Fran/Proyecto Tron/Tron 0.3.2/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("PlayZone");
+    }
+
+    public void OptionsMenu()
+    {
+        SceneManager.LoadScene("OptionsMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+    }
+}
